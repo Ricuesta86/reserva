5a1da4fd31b376477affe1ab6e521be4ef2fcb14
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-09-04 07:36:56 -0400

    Add imagen al header

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -1,20 +1,29 @@
 import { ReactNode } from 'react';
 
+import { useRouter } from 'next/router';
+
 type IHeroOneButtonProps = {
   title: ReactNode;
   description: string;
-  button: ReactNode;
+  // button: ReactNode;
 };
 
-const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center">
-    <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
-      {props.title}
-    </h1>
-    <div className="text-2xl mt-4 mb-16">{props.description}</div>
-
-    {props.button}
-  </header>
-);
+const HeroOneButton = (props: IHeroOneButtonProps) => {
+  const router = useRouter();
+  return (
+    <header className="text-center">
+      <img
+        src={`${router.basePath}/assets/images/imagen3.jpg`}
+        alt="Hotel"
+        className="inline"
+      />
+      <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
+        {props.title}
+      </h1>
+      <div className="text-2xl mt-4 mb-16">{props.description}</div>
+      {/* {props.button} */}
+    </header>
+  );
+};
 
 export { HeroOneButton };
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 import { Background } from '../background/Background';
-import { Button } from '../button/Button';
+// import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
@@ -34,24 +34,24 @@ const Hero = () => (
       </NavbarTwoColumns>
     </Section>
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
-          </>
-        }
-        description="The easiest way to build a React landing page in seconds."
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <a>
-              <Button xl>Download Your Free Theme</Button>
-            </a>
-          </Link>
-        }
-      />
-    </Section>
+    {/* <Section yPadding="pt-20 pb-32"> */}
+    <HeroOneButton
+      title={
+        <>
+          {'¿Quieres reservar en los Hoteles de Cuba?\n'}
+          <span className="text-primary-500">¡Te ayudamos! Contactanos</span>
+        </>
+      }
+      description="Nosotros hacemos todas las gestiones necesarias para que usted vaya al hotel deseado."
+      // button={
+      //   <Link href="https://creativedesignsguru.com/category/nextjs/">
+      //     <a>
+      //       <Button xl>Download Your Free Theme</Button>
+      //     </a>
+      //   </Link>
+      // }
+    />
+    {/* </Section> */}
   </Background>
 );
 
